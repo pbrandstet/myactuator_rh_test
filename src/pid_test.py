0a02e1a164a20ab7d6247e248f11d0ff8df86006
pbrandstet <peter.b90r@gmail.com>
2024-07-08 13:54:10 +0200

    added more params to pid test

--- src/pid_test.py
+++ src/pid_test.py
@@ -3,17 +3,31 @@ import matplotlib.pyplot as plt
 import myactuator_rh_py as rh
 import time
 
-driver = rh.CanDriver("can1")
+driver = rh.CanDriver("can0")
 actuator = rh.ActuatorInterface(driver, 23)
 
 actuator.setMotorDisabled()
 
 # Set the actuator to position control params
-actuator.setPidParameterPositionRingP(100.0)
+actuator.setPidParameterPositionRingP(10.0)
 actuator.setPidParameterPositionRingI(0.0)
-actuator.setPidParameterPositionRingD(80.0)
+actuator.setPidParameterPositionRingD(1.1)
+actuator.setPidParameterPositionLoopSlope(0.0)
+actuator.setPidParameterPositionLoopFilterPeriod(0.01)
 
-actuator.setRestrictionParameterMotorSpeedLimit(20)
+actuator.setPidParameterQAxisCurrentLoopP(2.0)
+actuator.setPidParameterQAxisCurrentLoopI(0.0)
+actuator.setPidParameterQAxisCurrentLoopD(0.0)
+actuator.setPidParameterQAxisCurrentLoopSlope(40.0)
+actuator.setPidParameterQAxisCurrentLoopFilterPeriod(0.01)
+
+actuator.setPidParameterDAxisCurrentLoopP(2.0)
+actuator.setPidParameterDAxisCurrentLoopI(0.0)
+actuator.setPidParameterDAxisCurrentLoopD(0.0)
+actuator.setPidParameterDAxisCurrentLoopSlope(40.0)
+actuator.setPidParameterDAxisCurrentLoopFilterPeriod(0.01)
+
+actuator.setRestrictionParameterMotorSpeedLimit(25)
 
 actuator.setPositionMode()
 
@@ -27,9 +41,9 @@ actuator.setSinglePointOperation(45.0)
 timestamps = []
 position = []
 for i in range(100):
-    timestamps.append(i * 0.1)
+    timestamps.append(i * 0.02)
     position.append(actuator.getCurrentPosition())
-    time.sleep(0.1)
+    time.sleep(0.02)
 
 actuator.setMotorDisabled()
 
